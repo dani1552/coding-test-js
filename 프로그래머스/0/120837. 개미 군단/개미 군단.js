ed72e5d7a3009e470957298592b377ab89794187
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-10-26 21:48:36 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,7 @@
+function solution(hp) {
+    const general = Math.floor(hp/5);
+    const soldier = Math.floor((hp%5)/3);
+    const worker = hp % 5 % 3;
+
+    return general + soldier + worker;
+}
