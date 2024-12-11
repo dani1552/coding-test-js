489a2496a845084b56b90cdaad18871be82931b8
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2024-12-11 13:51:25 +0900

    [Bronze IV] Title: 주사위 세개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,9 @@
+dice = list(map(int, input().split()))
+dice.sort()
+
+if dice[0] == dice[1] == dice[2]:
+    print(10000 + dice[0] * 1000)
+elif dice[0] == dice[1] or dice[1] == dice[2] or dice[0] == dice[2]:
+    print(1000 + dice[1] * 100)
+else:
+    print(dice[2] * 100)
