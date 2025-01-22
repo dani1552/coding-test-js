51139de1d05d3d3bea4e91db7c8dea936e8ccecb
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-01-22 14:04:50 +0900

    [Bronze III] Title: 아 맞다 마늘, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.py
+++ 백준/Bronze/32978. 아 맞다 마늘/아 맞다 마늘.py
@@ -1,9 +1,7 @@
 n = int(input())
 ingredient = list(map(str, input().split()))
-use_ingredient = list(map(str, input().split()))
+use_ingredient = set(map(str, input().split()))
 
-for i in range(n):
-    if ingredient[i] in use_ingredient:
-        continue
-    else:
-        print(ingredient[i])
+for item in ingredient:
+    if item not in use_ingredient:
+        print(item)
