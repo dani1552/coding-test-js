bfb8e5a63a3f47c394d23ac47b486ba92af630c6
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-06-03 16:49:16 +0900

    [Silver III] Title: 222-풀링, Time: 424 ms, Memory: 73960 KB -BaekjoonHub

---
+++ 백준/Silver/17829. 222－풀링/222－풀링.py
@@ -0,0 +1,25 @@
+n = int(input())
+nums = []
+
+for i in range(n):
+    temp = list(map(int, input().split()))
+    nums.append(temp)
+
+def pooling(size, x, y):
+    mid = size // 2
+
+    if size == 2:
+        answer = [nums[x][y], nums[x][y+mid], nums[x+mid][y], nums[x+mid][y+mid]]
+        answer.sort()
+        return answer[-2]
+    
+    lt = pooling(mid, x, y)
+    rt = pooling(mid, x+mid, y)
+    lb = pooling(mid, x, y+mid)
+    rb = pooling(mid, x+mid, y+mid)
+
+    answer = [lt, rt, lb, rb]
+    answer.sort()
+    return answer[-2]
+
+print(pooling(n, 0, 0))
