9c48c74d9669cb7de8c74849374d78c778cade27
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-05 15:39:02 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 192 ms, Memory: 9664 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,12 @@
+const input = Number(
+  require("fs").readFileSync("/dev/stdin").toString().trim()
+);
+
+function fibo(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibo(n - 1) + fibo(n - 2);
+}
+
+console.log(fibo(input));
