3782ac3a8ecb32e99bfe76b0b01b77c6ffa7bbc2
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-08 16:43:36 +0900

    [level 0] Title: 가위 바위 보, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,11 +1,4 @@
 function solution(rsp) {
-    let answer = "";
-    
-    for(let i=0; i<rsp.length; i++) {
-        if(rsp[i] == '0') answer += '5';
-        if(rsp[i] == '2') answer += '0';
-        if(rsp[i] == '5') answer += '2';
-    }
-    
+    var answer = '';
     return answer;
 }
