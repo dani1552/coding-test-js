846193de1e00aad829ba1016eb3ddd39fa8a6cc7
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-08 17:05:30 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let answer = [];
+    
+    for(let i=0; i<my_string.length; i++) {
+        if (!isNaN(my_string[i]) && my_string[i] !== ' ') {
+            answer.push(Number(my_string[i]));
+        }
+    }
+    return answer.sort((a, b) => a - b);
+}
