e3b6e87da0580f7f69bfa3c40e80799abafb25cb
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-09 17:19:34 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,13 +1,4 @@
 function solution(my_string) {
-    let result = '';
-    
-    for(let word of my_string) {
-        if (word === word.toUpperCase()) {
-            result += word.toLowerCase();
-        } else {
-            result += word.toUpperCase();
-        }
-    }
-    
-    return result;
+    var answer = '';
+    return answer;
 }
