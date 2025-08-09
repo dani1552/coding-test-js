083984d91975c571aa1d11fc3d081e70816c8616
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-09 17:33:38 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,12 @@
+function solution(my_string, num1, num2) {
+    let arr = my_string.split('');
+
+    let temp = arr[num1];
+    arr[num1] = arr[num2];
+    arr[num2] = temp;
+    
+    return arr.join('');
+}
+
+// 문자열 -> 배열 .split('')
+// 배열 -> 문자열 .join('')
