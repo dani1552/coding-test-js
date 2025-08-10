939c90bf889f12bff714c4b6e94962df2545221f
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-10 14:19:28 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,6 +1,6 @@
 function solution(age) {
   return String(age)
-    .split('')                          // ['2','3']
-    .map(d => String.fromCharCode(97 + Number(d))) // 97은 'a'의 코드
-    .join('');                          // 'cd'
+    .split('')                        
+    .map(d => String.fromCharCode(97 + Number(d))) 
+    .join('');                      
 }
