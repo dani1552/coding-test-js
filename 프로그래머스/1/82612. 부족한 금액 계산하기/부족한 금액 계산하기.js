62a95804bab7e984d646f23189a8967aa4ab9394
Daeun Kim <150661115+dani1552@users.noreply.github.com>
2025-08-27 16:09:49 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,10 @@
+function solution(price, money, count) {
+    let sum = 0;
+    
+    for(let i=1; i<=count; i++) {
+        sum += (price * i);
+    }
+    
+    let answer = sum - money;
+    return answer > 0 ? answer : 0;
+}
