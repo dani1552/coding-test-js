d32ef9b67349878491ca95c1cddfde1a13ec31d0
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-09-26 14:01:51 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.37 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 71.5 MB, 시간: 89.04 ms
+메모리: 66 MB, 시간: 64.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 26일 14:38:39
+2025년 09월 26일 14:01:45
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,24 +1,20 @@
 function solution(participant, completion) {
-    const map = {}
+    const participantCount = new Map();
     
-    // 마라톤 경기에 참여한 선수의 이름 등장 횟수 저장
-    for (let name of participant) {
-        if (map[name]) {
-            map[name]++;
-        } else {
-            map[name] = 1;
-        }
+    for(let name of participant) {
+        const count = participantCount.get(name) || 0;
+        participantCount.set(name, count + 1);
     }
     
-    // 완주한 사람 이름을 해시맵에서 차감
-    for (let name of completion) {
-        map[name]--;
+    for(let name of completion) {
+        participantCount.set(name, participantCount.get(name) - 1);
     }
     
-    // 값이 1인 사람(완주하지 못한 사람)을 찾아서 반환
-    for (let name in map) {
-        if (map[name] > 0) {
+    for(let [name, count] of participantCount) {
+        if(count >= 1) {
             return name;
         }
     }
+    
+    return '';
 }
