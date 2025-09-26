6d0b894e09722c68f4668b933f5d5fa00136169b
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-09-26 14:02:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 64.37 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,20 +1,11 @@
 function solution(participant, completion) {
-    const participantCount = new Map();
+    let nocomplete = [];
     
     for(let name of participant) {
-        const count = participantCount.get(name) || 0;
-        participantCount.set(name, count + 1);
-    }
-    
-    for(let name of completion) {
-        participantCount.set(name, participantCount.get(name) - 1);
-    }
-    
-    for(let [name, count] of participantCount) {
-        if(count >= 1) {
-            return name;
+        if(!completion.includes(name)) {
+            nocomplete.push(name);
         }
     }
     
-    return '';
+    return nocomplete;
 }
