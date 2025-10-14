32b3c7e006d0b9bf124ff5c920319ced74309665
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-10-14 22:14:56 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,26 @@
+function solution(keyinput, board) {
+    let x = 0;
+    let y = 0;
+    
+    const maxX = Math.floor(board[0] / 2);
+    const maxY = Math.floor(board[1] / 2);
+    
+    for (let key of keyinput) {
+        switch (key) {
+            case "up":
+                if (y < maxY) y += 1;
+                break;
+            case "down":
+                if(y > -maxY) y-= 1;
+                break;
+            case "left":
+                if(x > -maxX) x -= 1;
+                break;
+            case "right":
+                if (x < maxX) x += 1;
+                break;
+        }
+    }
+    
+    return [x, y];
+}
