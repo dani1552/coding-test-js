3f88c8a404fb7eb39ebe329ba273768f2bb440b1
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-03 12:03:21 +0900

    [level 2] Title: 기능개발, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,19 @@
+function solution(progresses, speeds) {
+    const days = progresses.map((p, i) => Math.ceil((100 - p) / speeds[i]));
+    const result = [];
+    let maxDay = days[0];
+    let count = 1;
+    
+    for (let i = 1; i < days.length; i++) {
+        if (days[i] <= maxDay) {
+            count++;
+        } else {
+            result.push(count);
+            count = 1;
+            maxDay = days[i];
+        }
+    }
+    
+    result.push(count);
+    return result;
+}
