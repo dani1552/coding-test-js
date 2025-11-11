5aae49352f0b9b59d3c74a315d5dd2e249a782e5
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-11 10:23:56 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,6 +1,8 @@
 function solution(num_list) {
-    for (let n of num_list) {
-        if (n < 0) return num_list.indexOf(n);
-    }
-    return -1;
+    // for (let n of num_list) {
+    //     if (n < 0) return num_list.indexOf(n);
+    // }
+    // return -1;
+    
+    return num_list.findIndex(n => n < 0);
 }
