234e7a2bff6c0d0bc17e1b72c1e3b713116c04c8
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-11 10:33:08 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let result = [];
+    
+    while (n != 1) {
+        result.push(n);
+        if (n % 2 === 0) n /= 2;
+        else n = (3 * n + 1);
+    }
+    
+    return [...result, 1];
+}
