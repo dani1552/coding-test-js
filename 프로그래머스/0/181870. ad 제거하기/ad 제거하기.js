0d8780264340c54febd87fa0974ac011607d5bdd
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-11 11:36:54 +0900

    [level 0] Title: ad 제거하기, Time: 0.25 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.js
@@ -0,0 +1,12 @@
+function solution(strArr) {
+    let result = [];
+    for (let s of strArr) {
+        if (s.includes("ad")) {
+            result.push();
+        } else {
+            result.push(s);
+        }
+    }
+    
+    return result;
+}
