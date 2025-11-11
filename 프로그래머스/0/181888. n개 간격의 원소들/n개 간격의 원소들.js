04018c9be262a20507cd51e2ba85f7883662a3f2
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-11 10:02:46 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,8 +1,11 @@
 function solution(num_list, n) {
-    let temp = [];
-    for (let i = 0; i < num_list.length; i += n) {
-        temp.push(num_list[i]);
-    }
+//     let temp = [];
+//     for (let i = 0; i < num_list.length; i += n) {
+//         temp.push(num_list[i]);
+//     }
     
-    return temp;
+//     return temp;
+    return num_list.filter((_, i) => i % n === 0);
 }
+
+
