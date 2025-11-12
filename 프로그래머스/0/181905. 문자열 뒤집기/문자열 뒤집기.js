faa16cc774f8e5d274b27b0055563830a8297d7e
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-12 14:02:41 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, s, e) {
+    const origin = my_string.slice(s, e + 1);
+    const reversed = origin.split("").reverse().join("");
+    
+    return my_string.slice(0, s) + reversed + my_string.slice(e + 1);
+}
