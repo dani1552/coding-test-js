67c843877e8cb1b388c7e99c6f019ef75d593c9d
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-12 13:52:15 +0900

    [level 0] Title: 1로 만들기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,17 @@
+function solution(num_list) {
+    let count = 0;
+    
+    for (let n of num_list) {    
+        while (n != 1) {
+            if (n % 2 === 0) {
+                n /= 2;
+                count++;
+            } else {
+                n = (n - 1) / 2;
+                count++;
+            }
+        }
+    }
+    
+    return count;
+}
