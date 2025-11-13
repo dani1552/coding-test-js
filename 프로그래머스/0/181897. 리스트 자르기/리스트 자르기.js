d078b2bda933bd6fd7be44caf3f0e83865fdcf28
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-13 10:37:28 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,20 @@
+function solution(n, slicer, num_list) {
+    const a = slicer[0];
+    const b = slicer[1];
+    const c = slicer[2];
+    
+    if (n === 1) {
+        return num_list.slice(0, b + 1);
+    } else if (n === 2) {
+        return num_list.slice(a);
+    } else if (n === 3) {
+        return num_list.slice(a, b + 1);
+    } else if (n === 4) {
+        let temp = [];
+        for (let i = a; i <= b; i += c) {
+            temp.push(num_list[i]);
+        }
+        
+        return temp;
+    }
+}
