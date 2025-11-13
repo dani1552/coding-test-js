5c841b592bbf20e44f7b8bdf8800e7da81199127
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2025-11-13 11:30:55 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 44.32 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    const str = input[0];
+    
+    let result = '';
+
+    for (let ch of str) {
+        if (ch >= 'a' && ch <= 'z') {
+            result += ch.toUpperCase();
+        } else {
+            result += ch.toLowerCase();
+        }
+    }
+
+    console.log(result);
+});
