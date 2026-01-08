f96d6c782359af21c1f344e5ce6edf19bfe22318
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2026-01-08 09:20:50 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
+
+const n = Number(input[0]);
+const m = Number(input[1]);
+
+// 인접 리스트
+const graph = Array.from({ length: n + 1 }, () => []);
+for (let i = 2; i < 2 + m; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+  graph[a].push(b);
+  graph[b].push(a);
+}
+
+const visited = Array(n + 1).fill(false);
+let count = 0;
+
+function dfs(v) {
+  visited[v] = true;
+  for (const next of graph[v]) {
+    if (!visited[next]) {
+      count++;
+      dfs(next);
+    }
+  }
+}
+
+dfs(1);
+console.log(count);
