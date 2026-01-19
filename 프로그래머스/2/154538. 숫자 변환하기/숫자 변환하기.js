6bfb56a5405ba5d5056f271f45fc2d7a9e9d11d5
김다은 (Daeun Kim) <150661115+dani1552@users.noreply.github.com>
2026-01-19 19:14:14 +0900

    [level 2] Title: 숫자 변환하기, Time: 31.27 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,28 @@
+function solution(x, y, n) {
+  const dp = Array(y + 1).fill(-1);
+  dp[x] = 0;
+
+  for (let i = x; i <= y; i++) {
+    if (dp[i] === -1) continue;
+
+    if (i + n <= y) {
+      if (dp[i + n] === -1 || dp[i + n] > dp[i] + 1) {
+        dp[i + n] = dp[i] + 1;
+      }
+    }
+
+    if (i * 2 <= y) {
+      if (dp[i * 2] === -1 || dp[i * 2] > dp[i] + 1) {
+        dp[i * 2] = dp[i] + 1;
+      }
+    }
+
+    if (i * 3 <= y) {
+      if (dp[i * 3] === -1 || dp[i * 3] > dp[i] + 1) {
+        dp[i * 3] = dp[i] + 1;
+      }
+    }
+  }
+
+  return dp[y];
+}
